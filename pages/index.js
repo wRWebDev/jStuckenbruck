d154e9040180db62c946cf6bd987f6473a96b540
wRWebDev <wrobertson@hd9host.com>
2021-01-19 03:06:31 +0000

    Initial commit - experiment with lots of carousels

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,34 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import firebase from '../db/firebase'
+import HouseStyle from '../components/Layout/HouseStyle'
+import Content from '../components/Content/Homepage'
 
-export default function Home() {
+const pageDetails = {
+  name: 'home',
+  title: 'Home',
+  description: 'Welcome to the website of conductor, Johann Stuckenbruck.',
+  image: '',
+  darkMode: true,
+  hideFooter: true
+}
+
+const Page = ({ content }) => {
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <>
+      <HouseStyle properties={pageDetails}>
+        <Content content={content} />
+      </HouseStyle>
+    </>
   )
 }
+
+export async function getServerSideProps(ctx){
+  const pageData = await firebase
+    .firestore()
+    .collection('pages')
+    .doc(pageDetails.name)
+    .get()
+  return { props: { content: pageData.data() } }
+}
+
+export default Page
