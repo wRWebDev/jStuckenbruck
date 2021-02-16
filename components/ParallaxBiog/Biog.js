b3af69824dd920628d57369c121e19c901a14e4c
wRWebDev <wrobertson@hd9host.com>
2021-02-16 23:05:24 +0000

    Biog emboldened

--- components/ParallaxBiog/Biog.js
+++ components/ParallaxBiog/Biog.js
@@ -1,8 +1,13 @@
 import Quote from './Quote'
 import styles from './styles/ParallaxBiog.module.css'
+import reactStringReplace from 'react-string-replace'
 
 export default function Biog({ bio = String, quote = Object }){
 
+    const highlightBio = string =>
+        reactStringReplace(string, /\[+(.*?)\]+/g, (match, i) => (
+            <strong key={i}>{match}</strong>
+        ))
     const paragraphs = bio.split('\n')
 
     return (
@@ -11,11 +16,11 @@ export default function Biog({ bio = String, quote = Object }){
                 <section 
                     className={`${styles.biogSection} ${styles.biogSection}`}
                 >
-                    <span>{paragraphs.slice(0, paragraphs.length / 2).join('\n')}</span>
+                    <span>{highlightBio(paragraphs.slice(0, paragraphs.length / 2).join('\n'))}</span>
                 </section>
                 <Quote quote={quote} />
                 <section className={`${styles.biogSection} ${styles.biogSection}`}>
-                    <span>{paragraphs.slice(paragraphs.length / 2).join('\n')}</span>
+                    <span>{highlightBio(paragraphs.slice(paragraphs.length / 2).join('\n'))}</span>
                 </section>
             </article>
         </>
