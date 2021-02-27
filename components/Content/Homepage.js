55b9df245fa34b3de8b892345143e1baa1807bc7
wRWebDev <wrobertson@hd9host.com>
2021-02-27 18:35:17 +0000

    Animated biog preview

--- components/Content/Homepage.js
+++ components/Content/Homepage.js
@@ -5,28 +5,90 @@
 */
 
 import FullPageCarousel from '../FullPageCarousel'
+import Image from 'next/image'
 import Link from 'next/link'
+import ContactForm from '../ContactForm'
+import reactStringReplace from 'react-string-replace'
+import AOS from 'aos'
+import { useEffect } from 'react'
 
-const Content = ({ content }) => {
+/* 
+    Function to wrap any words from inside [square brackets] 
+    into <strong> tags 
+*/
+const highlightBio = string =>
+reactStringReplace(string, /\[+(.*?)\]+/g, (match, i) => (
+    <strong key={i}>{match}</strong>
+))
+
+const Content = ({ content, biography }) => {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000
+        })
+    }, [])
 
     const { images} = content
 
+    const firstPara = biography.body.split('\n')[0]
 
     return (
         <>
+            {/* Hero Section */}
             <FullPageCarousel
                 folder="homepage-carousel"
                 images={images}
             >
-                <Link href="/biography">
-                    <a>
-                        <button id="home-button">
-                            <h4>Click to enter</h4>
-                            <img src="/img/house/right-arrow.svg" />
-                        </button>
-                    </a>
-                </Link>
+                <div className="scrollDown">
+                    <span />
+                    <span />
+                    <span />
+                </div>
             </FullPageCarousel>
+
+            {/* Biography */}
+            <section className="homepageSection biog">
+                <div 
+                    className="image"
+                    data-aos="fade-right"    
+                >
+                    <Image 
+                        src={`${process.env.NEXT_PUBLIC_BUCKET}/media/images/closeup.jpg`}
+                        layout="responsive"
+                        width={250}
+                        height={250}
+                    />
+                </div>
+                <article>
+                    <h2 data-aos="fade-left">Johann Stuckenbruck</h2>
+                    <p data-aos="fade-left">
+                        {highlightBio(firstPara)}
+                    </p>
+                    <div data-aos="fade-up">
+                        <Link href="/biography">
+                            <button>
+                                Read more
+                            </button>
+                        </Link>
+                    </div>
+                </article>
+                <div 
+                    className="scrollDown" 
+                    style={{filter: 'invert(1)'}}
+                    data-aos="fade-down"
+                >
+                    <span /><span /><span />
+                </div>
+            </section>
+            
+            {/* Next event */}
+            <section className="homepageSection event">
+                Event here
+            </section>
+            <section className="homepageSection contact">
+                <ContactForm />
+            </section>
         </>
     )
 }
